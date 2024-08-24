23b1233167fcb8a4a713be33100ffeebedf448a5
jatin-3009 <jatinninawe02@gmail.com>
2024-08-24 23:27:37 +0530

    Downloaded an empty spring boot project with lombok and spring web mvc dependencies

---
+++ src/main/java/com/project/exam_registration/ExamRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.project.exam_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamRegistrationApplication.class, args);
+	}
+
+}
